8b09633f2027b0076839ea940d0efcec7f4d50c8
constantinh5@gmail.com <constantinh5@gmail.com>
2021-06-08 22:01:30 +0200

    added start of ssh

--- public/basicInstall.sh
+++ public/basicInstall.sh
@@ -12,9 +12,13 @@ sudo apt-get install xrdp -y
 
 # ftp utils
 echo
-echo "Will try to install pure-ftpd"
+echo "Will try to install pure-ftpd and enable+start ssh"
 echo 
 sudo apt install pure-ftpd -y
+sudo systemctl start ssh
+sudo systemctl enable ssh
+sudo systemctl is-enabled ssh
+sudo systemctl status ssh
 
 echo
 echo "That is the current network interface configuration"
